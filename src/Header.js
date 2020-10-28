43372feb35f66911342baeb7d763e11fcad7158e
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-28 16:44:32 -0700

    added some styling

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                Welcome to PokeDex!
+                Welcome to PokeDex!  Pick Your Poke!
             </div>
         )
     }
